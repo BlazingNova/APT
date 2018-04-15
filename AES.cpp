07ce2bacafef741b872100ef2961d7264b20e7e1
mathew <mathewlionking@bitbucket.org>
2018-04-15 17:46:09 -0400

    Created AES Class

--- AES.cpp
+++ AES.cpp
@@ -1,7 +1,25 @@
 //============================================================================
 // Name        : AES.cpp
-// Author      : Mathew Prabakar
-// Version     : 1.0
-// Copyright   : Keep your hands off my code
-// Description : AES C++,
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
 //============================================================================
+
+#include <iostream>
+using namespace std;
+
+class AES{
+
+	uint8_t KEY[16];
+	vector<uint32_t> Text;
+	vector<uint32_t> W;
+
+};
+
+int main() {
+
+
+
+	return 0;
+}
